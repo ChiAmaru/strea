e1dfced1417f8e16cc5c6ab8c2c45916927dde42
ChiAmaru <144146052+ChiAmaru@users.noreply.github.com>
2023-09-05 12:20:19 -0400

    initial commit

---
+++ ja.js
@@ -0,0 +1,10 @@
+// Agregamos una animación suave para desplazarse a las secciones
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
